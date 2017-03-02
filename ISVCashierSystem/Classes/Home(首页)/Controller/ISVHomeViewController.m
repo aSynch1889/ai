6b58f250790a9b02f7b85d9db44882e127fc86a6
aihua <liuaihuajm@126.com>
2017-03-02 23:55:02 +0800

    添加home/me

---
+++ ISVCashierSystem/Classes/Home(首页)/Controller/ISVHomeViewController.m
@@ -0,0 +1,28 @@
+//
+//  ISVHomeViewController.m
+//  ISVCashierSystem
+//
+//  Created by aaaa on 17/3/2.
+//  Copyright © 2017年 ISV Co.,Ltd. All rights reserved.
+//
+
+#import "ISVHomeViewController.h"
+
+@interface ISVHomeViewController ()
+
+@end
+
+@implementation ISVHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ISVCashierSystem/Classes/Me(我的)/Controller/ISVMeViewController.m
@@ -0,0 +1,28 @@
+//
+//  ISVMeViewController.m
+//  ISVCashierSystem
+//
+//  Created by aaaa on 17/3/2.
+//  Copyright © 2017年 ISV Co.,Ltd. All rights reserved.
+//
+
+#import "ISVMeViewController.h"
+
+@interface ISVMeViewController ()
+
+@end
+
+@implementation ISVMeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
