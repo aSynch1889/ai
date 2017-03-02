b76ab9ee08a595bf10580819be3bb4564ea93d78
aihua <liuaihuajm@126.com>
2017-03-02 22:14:58 +0800

    init

--- ISVCashierSystem/ViewController.m
+++ ISVCashierSystem/ViewController.m
@@ -10,9 +10,29 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
+
+- (void)viewWillAppear:(BOOL)animated{
+    // Called when the view is about to made visible. Default does nothing
+    
+    [super viewWillAppear:animated];
+    
+    //去除导航栏下方的横线
+    
+//    [self.navigationBar setBackgroundImage:[UIImage imageWithColor:[self colorFromHexRGB:@"33cccc"]]
+//     
+//                       forBarPosition:UIBarPositionAny
+//     
+//                           barMetrics:UIBarMetricsDefault];
+//    
+//    [navigationBar setShadowImage:[UIImage new]];
+    
+    
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
