99ab5186601b032ce56dd8a3355ba7c5bca16ba4
sunny华子 <aizi1989010@126.com>
2017-03-08 10:53:33 +0800

    修改boundid

---
+++ ISVCashierSystem/Classes/Home(首页)/View/salesAnalysisView.h
@@ -0,0 +1,16 @@
+//
+//  salesAnalysisView.h
+//  ISVCashierSystem
+//
+//  Created by aaaa on 17/3/6.
+//  Copyright © 2017年 ISV Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface salesAnalysisView : UIView
+
+@property (nonatomic, strong)UILabel *titleLabel;//!<  销售总额
+@property (nonatomic, strong)UIImageView *imgView;//!<  金额图片
+
+@end
---
+++ ISVCashierSystem/Classes/Home(首页)/View/salesAnalysisView.m
@@ -0,0 +1,15 @@
+//
+//  salesAnalysisView.m
+//  ISVCashierSystem
+//
+//  Created by aaaa on 17/3/6.
+//  Copyright © 2017年 ISV Co.,Ltd. All rights reserved.
+//
+
+#import "salesAnalysisView.h"
+
+@implementation salesAnalysisView
+
+
+
+@end
