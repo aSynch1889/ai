dad6ac168f0a7fcac5262ea08dfeca170faa751b
sunny华子 <aizi1989010@126.com>
2017-03-08 16:27:38 +0800

    修改二维码扫描临时保存

--- ISVCashierSystem/Classes/Commom/ISVQRCode/HMScan/ISVScanViewController.m
+++ ISVCashierSystem/Classes/Commom/ISVQRCode/HMScan/ISVScanViewController.m
@@ -25,6 +25,7 @@
 @property (nonatomic, strong) AVCaptureSession *session;
 @property (nonatomic, strong) AVCaptureDevice *device;
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer *preview;
+@property (nonatomic, weak) UIImageView *line;
 //@property (nonatomic, weak) IBOutlet UIButton *openTorchButton;// 闪关灯
 
 @end
@@ -46,18 +47,8 @@
     // 获取读取读取二维码的会话
     self.session = [AVCaptureSession readQRCodeWithMetadataObjectsDelegate:self];
     
-    // 扫描识别范围
-    AVCaptureMetadataOutput *output = [[self.session outputs] firstObject];
-    output.rectOfInterest = CGRectMake((self.view.bounds.size.height - previewWH) * 0.5 / self.view.bounds.size.height,(self.view.bounds.size.width - previewWH) * 0.5  / self.view.bounds.size.width,previewWH / self.view.bounds.size.height,previewWH / self.view.bounds.size.width);
-    
-    // 创建预览图层
-    AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
-    previewLayer.backgroundColor = [UIColor redColor].CGColor;
-    previewLayer.frame = self.view.bounds;
-    [previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
-    [self.view.layer insertSublayer:previewLayer atIndex:0];
-    
     [self setUpNav];
+    [self creatControl];
     //设置参数
     [self setupCamera];
 }
@@ -75,8 +66,83 @@
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"相册" style:UIBarButtonItemStylePlain target:self action:@selector(rightItemClick)];
 }
 
+
+- (void)creatControl
+{
+    CGFloat scanW = kSCREEN_WIDTH * 0.65;
+    CGFloat padding = 10.0f;
+    CGFloat labelH = 20.0f;
+    CGFloat tabBarH = 64.0f;
+    CGFloat cornerW = 26.0f;
+    CGFloat marginX = (kSCREEN_WIDTH - scanW) * 0.5;
+    CGFloat marginY = (kSCREEN_HEIGHT - scanW - padding - labelH) * 0.5;
+    
+    //遮盖视图
+    for (int i = 0; i < 4; i++) {
+        UIView *cover = [[UIView alloc] initWithFrame:CGRectMake(0, (marginY + scanW) * i, kSCREEN_WIDTH, marginY + (padding + labelH) * i)];
+        if (i == 2 || i == 3) {
+            cover.frame = CGRectMake((marginX + scanW) * (i - 2), marginY, marginX, scanW);
+        }
+        cover.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5f];
+        [self.view addSubview:cover];
+    }
+    
+    //扫描视图
+    UIView *scanView = [[UIView alloc] initWithFrame:CGRectMake(marginX, marginY, scanW, scanW)];
+    [self.view addSubview:scanView];
+    
+    //扫描线
+    UIImageView *line = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, scanW, 2)];
+    [self drawLineForImageView:line];
+    [scanView addSubview:line];
+    self.line = line;
+    
+    //边框
+    UIView *borderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, scanW, scanW)];
+    borderView.layer.borderColor = [[UIColor whiteColor] CGColor];
+    borderView.layer.borderWidth = 1.0f;
+    [scanView addSubview:borderView];
+    
+    //扫描视图四个角
+    for (int i = 0; i < 4; i++) {
+        CGFloat imgViewX = (scanW - cornerW) * (i % 2);
+        CGFloat imgViewY = (scanW - cornerW) * (i / 2);
+        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(imgViewX, imgViewY, cornerW, cornerW)];
+        if (i == 0 || i == 1) {
+            imgView.transform = CGAffineTransformRotate(imgView.transform, M_PI_2 * i);
+        }else {
+            imgView.transform = CGAffineTransformRotate(imgView.transform, - M_PI_2 * (i - 1));
+        }
+        [self drawImageForImageView:imgView];
+        [scanView addSubview:imgView];
+    }
+    
+    //提示标签
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(scanView.frame) + padding, kSCREEN_WIDTH, labelH)];
+    label.text = @"将二维码/条形码放入框内，即可自动扫描";
+    label.font = ISVFontSize(12);
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor whiteColor];
+    [self.view addSubview:label];
+    
+    //选项栏
+    UIView *tabBarView = [[UIView alloc] initWithFrame:CGRectMake(0, kSCREEN_HEIGHT - tabBarH, kSCREEN_WIDTH, tabBarH)];
+    tabBarView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8f];
+    [self.view addSubview:tabBarView];
+    
+    //开启照明按钮
+    UIButton *lightBtn = [[UIButton alloc] initWithFrame:CGRectMake(kSCREEN_WIDTH - 100, 0, 100, tabBarH)];
+    lightBtn.titleLabel.font = [UIFont systemFontOfSize:16.0f];
+//    [lightBtn setTitle:@"开启照明" forState:UIControlStateNormal];
+//    [lightBtn setTitle:@"关闭照明" forState:UIControlStateSelected];
+    [lightBtn setImage:[UIImage imageNamed:@"flashON"] forState:UIControlStateNormal];
+    [lightBtn addTarget:self action:@selector(openTorchButtonTouched:) forControlEvents:UIControlEventTouchUpInside];
+    [tabBarView addSubview:lightBtn];
+}
+
 - (void)setupCamera
 {
+
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         //初始化相机设备
         _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
@@ -94,8 +160,12 @@
         //高质量采集率
         [_session setSessionPreset:AVCaptureSessionPresetHigh];
         
-        if ([_session canAddInput:input]) [_session addInput:input];
-        if ([_session canAddOutput:output]) [_session addOutput:output];
+        if ([_session canAddInput:input]) {
+            [_session addInput:input];
+        }
+        if ([_session canAddOutput:output]) {
+            [_session addOutput:output];
+        }
         
         //条码类型（二维码/条形码）
         output.metadataObjectTypes = [NSArray arrayWithObjects:AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeQRCode, nil];
@@ -106,6 +176,8 @@
             _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
             _preview.frame = CGRectMake(0, 0, kSCREEN_WIDTH, kSCREEN_HEIGHT);
             [self.view.layer insertSublayer:_preview atIndex:0];
+            CGRect rect = CGRectMake((kSCREEN_WIDTH - 250) / 2, (kSCREEN_HEIGHT - 250 - 72)/2, 250, 250);
+            output.rectOfInterest = [_preview metadataOutputRectOfInterestForRect:rect];
             [_session startRunning];
         });
     });
@@ -221,7 +293,7 @@
     }
 }
 
-- (IBAction)openTorchButtonTouched:(id)sender {
+- (void)openTorchButtonTouched:(id)sender {
     
     UIButton *torchBtn = sender;
     BOOL isLightOpened = [self isLightOpened];
@@ -286,6 +358,63 @@
     }
     [pickerImageVC setTitle:@"相册"];
     [self presentViewController:pickerImageVC animated:YES completion:nil];
+}
+
+
+//绘制角图片
+- (void)drawImageForImageView:(UIImageView *)imageView
+{
+    UIGraphicsBeginImageContext(imageView.bounds.size);
+    
+    //获取上下文
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    //设置线条宽度
+    CGContextSetLineWidth(context, 6.0f);
+    //设置颜色
+    CGContextSetStrokeColorWithColor(context, [[UIColor greenColor] CGColor]);
+    //路径
+    CGContextBeginPath(context);
+    //设置起点坐标
+    CGContextMoveToPoint(context, 0, imageView.bounds.size.height);
+    //设置下一个点坐标
+    CGContextAddLineToPoint(context, 0, 0);
+    CGContextAddLineToPoint(context, imageView.bounds.size.width, 0);
+    //渲染，连接起点和下一个坐标点
+    CGContextStrokePath(context);
+    
+    imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+}
+
+//绘制线图片
+- (void)drawLineForImageView:(UIImageView *)imageView
+{
+    CGSize size = imageView.bounds.size;
+    UIGraphicsBeginImageContext(size);
+    
+    //获取上下文
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    //创建一个颜色空间
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //设置开始颜色
+    const CGFloat *startColorComponents = CGColorGetComponents([[UIColor greenColor] CGColor]);
+    //设置结束颜色
+    const CGFloat *endColorComponents = CGColorGetComponents([[UIColor whiteColor] CGColor]);
+    //颜色分量的强度值数组
+    CGFloat components[8] = {startColorComponents[0], startColorComponents[1], startColorComponents[2], startColorComponents[3], endColorComponents[0], endColorComponents[1], endColorComponents[2], endColorComponents[3]
+    };
+    //渐变系数数组
+    CGFloat locations[] = {0.0, 1.0};
+    //创建渐变对象
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 2);
+    //绘制渐变
+    CGContextDrawRadialGradient(context, gradient, CGPointMake(size.width * 0.5, size.height * 0.5), size.width * 0.25, CGPointMake(size.width * 0.5, size.height * 0.5), size.width * 0.5, kCGGradientDrawsBeforeStartLocation);
+    //释放
+    CGColorSpaceRelease(colorSpace);
+    CGGradientRelease(gradient);
+    
+    imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
 }
 
 @end
